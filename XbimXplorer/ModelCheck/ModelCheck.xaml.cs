2c960e4b65a5574c802e49b62b4975700fc17dfb
JenniferZh; <jenniferzhang0606@gmail.com>
2018-01-19 14:12:26 +0800

    v2.0

--- XbimXplorer/ModelCheck/ModelCheck.xaml.cs
+++ XbimXplorer/ModelCheck/ModelCheck.xaml.cs
@@ -17,6 +17,7 @@ using Xbim.Presentation.XplorerPluginSystem;
 using System.Xml;
 using System.Diagnostics;
 using System.IO;
+using System.ComponentModel;
 using Newtonsoft.Json;
 
 
@@ -36,6 +37,7 @@ namespace XbimXplorer.ModelCheck
         public ModelCheck()
         {
             InitializeComponent();
+            InitializeBackgroundWorker();
         }
 
         private IXbimXplorerPluginMasterWindow _parentWindow;
@@ -45,6 +47,13 @@ namespace XbimXplorer.ModelCheck
         public string WindowTitle => "模型检查";
         public string SplPath;
         public Data_ResultJson data_ResultJson = null;
+        private BackgroundWorker CheckWorker;
+
+        private void InitializeBackgroundWorker()
+        {
+            CheckWorker = new BackgroundWorker();
+            CheckWorker.DoWork += new DoWorkEventHandler(doCheck);
+        }
 
 
 
@@ -59,12 +68,6 @@ namespace XbimXplorer.ModelCheck
             // whole datacontext binding, see http://stackoverflow.com/questions/8343928/how-can-i-create-a-binding-in-code-behind-that-doesnt-specify-a-path
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            RuleContent.Text = typeof(ModelCheck).Assembly.Location;
-
-           // TxtOut.AppendText(_parentWindow.GetOpenedModelFileName());
-        }
 
         private void item_dbClick(object sender, RoutedEventArgs e)
         {
@@ -92,22 +95,11 @@ namespace XbimXplorer.ModelCheck
         }
 
 
-
-        private void btnTest_Click(object sender, RoutedEventArgs e)
-        {
-            string runtime_path = typeof(ModelCheck).Assembly.Location;
-            string dir = System.IO.Path.GetDirectoryName(runtime_path);
-
-            RuleContent.Text = dir;
-
-            //TxtOut.AppendText(ts); 
-        }
-
         private void btnXlsCheck_Click(object sender, RoutedEventArgs e)
         {
             //打开xls
             string xlsPath = null;
-            OpenFileDialog openFileDialog = new OpenFileDialog();
+            OpenFileDialog openFileDialog = new OpenFileDialog { Filter = "Rules Files|*.xls;*.xlsx" };
             if (openFileDialog.ShowDialog() == true)
             {
                 xlsPath = openFileDialog.FileName;
@@ -192,31 +184,46 @@ namespace XbimXplorer.ModelCheck
 
         }
 
-        int testcnt = 0;
 
         private void OutputHandler(object sendingProcess, DataReceivedEventArgs outline)
         {
-            testcnt++;
-            CheckLog.Logger("[" + testcnt + "]"+outline.Data);
-            
-            
-            //this.RuleContent.Dispatcher.Invoke(new Action(delegate
-            //{
-            //    this.RuleContent.Text = outline.Data;
-            //}));
+            if(outline.Data != null)
+                ResultProcessing(outline.Data);
         }
 
-        private void btnTestCheck_Click(object sender, RoutedEventArgs e)
+
+        private void btnCheck_Click(object sender, RoutedEventArgs e)
+        {
+            CheckWorker.RunWorkerAsync();
+        }
+
+
+        private void doCheck(object sender, DoWorkEventArgs e)
         {
             Process process = new Process();
             String datafrom = "ifc";
             String checkType = "ConsistencyCheck";
             String checkMode = "1";
-            String normPath = "E:\\1实验室工作\\SPLdoc\\rulechecker功能基准测试.spl";
-            String normSelector = "2.2.1;2.2.3";
-            //String normSelector = GetSelectedNorm();
-            //String normPath = SplPath;
+            //String normPath = "E:\\1实验室工作\\SPLdoc\\rulechecker功能基准测试.spl";
+            //String normSelector = "2.2.1;2.2.3";
             String modelPath = _parentWindow.GetOpenedModelFileName();
+            if (modelPath == null)
+            {
+                MessageBox.Show("请载入模型文件", "提示", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            String normPath = SplPath;
+            if (normPath == null)
+            {
+                MessageBox.Show("请载入规则文件", "提示", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            String normSelector = GetSelectedNorm();
+            if (normSelector.Equals(""))
+            {
+                MessageBox.Show("请选择规则", "提示", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
 
             process.StartInfo.FileName = Config_Global.DIR + "\\BC.exe";
             process.StartInfo.Arguments = " -datafrom " + datafrom + " -checktype " + checkType + " -checkmode " + checkMode + " -normpath " + "\"" + normPath + "\"" + " -normsel " + "\"" + normSelector + "\""; // Note the /c command (*)
@@ -238,115 +245,45 @@ namespace XbimXplorer.ModelCheck
 
 
             StreamWriter myinput = process.StandardInput;
-            //myinput.WriteLine(modelPath);
-            myinput.WriteLine("E:\\1实验室工作\\SPLdoc\\AC20-Institute-Var-2.ifc");
-            myinput.Close();
-            //* Read the output (or the error)
-            //string output = process.StandardOutput.ReadToEnd();
-            //Console.WriteLine(output);
-            //string err = process.StandardError.ReadToEnd();
-            //Console.WriteLine(err);
-
-            //CheckLog.Logger("[test]" + output);
-
-
-            //ResultProcessing(output);
-            process.WaitForExit();
-
-        }
-
-        private void btnCheck_Click(object sender, RoutedEventArgs e)
-        {
-            Process process = new Process();
-            String datafrom = "ifc";
-            String checkType = "ConsistencyCheck";
-            String checkMode = "1";
-            //String normPath = "E:\\1实验室工作\\SPLdoc\\rulechecker功能基准测试.spl";
-            //String normSelector = "2.2.1;2.2.3";
-            String normSelector = GetSelectedNorm();
-            String normPath = SplPath;
-            String modelPath = _parentWindow.GetOpenedModelFileName();
-
-
-            if (normSelector.Equals(""))
-            {
-                MessageBox.Show("请选择规则", "提示", MessageBoxButton.OK, MessageBoxImage.Information);
-            }
-            if(normPath == null)
-            {
-                MessageBox.Show("请载入规则文件", "提示", MessageBoxButton.OK, MessageBoxImage.Information);
-            }
-            if(modelPath == null)
-            {
-                MessageBox.Show("请载入模型文件", "提示", MessageBoxButton.OK, MessageBoxImage.Information);
-            }
-
-            
-            process.StartInfo.FileName = Config_Global.DIR+"\\BC.exe";
-            process.StartInfo.Arguments = " -datafrom " + datafrom + " -checktype " + checkType + " -checkmode " + checkMode + " -normpath " + "\"" +normPath +"\"" +" -normsel " +"\"" +normSelector + "\""; // Note the /c command (*)
-            process.StartInfo.UseShellExecute = false;
-            process.StartInfo.RedirectStandardInput = true;
-            process.StartInfo.RedirectStandardOutput = true;
-            process.StartInfo.RedirectStandardError = true;
-            process.StartInfo.CreateNoWindow = true;
-
-
-            //process.OutputDataReceived += new DataReceivedEventHandler(OutputHandler);
-            //process.ErrorDataReceived += new DataReceivedEventHandler(OutputHandler);
-
-            
-
-            process.Start();
-            //process.BeginOutputReadLine();
-            //process.BeginErrorReadLine();
-
-
-            StreamWriter myinput = process.StandardInput;
             myinput.WriteLine(modelPath);
             //myinput.WriteLine("E:\\1实验室工作\\SPLdoc\\AC20-Institute-Var-2.ifc");
             myinput.Close();
-            //* Read the output (or the error)
-            string output = process.StandardOutput.ReadToEnd();
-            //Console.WriteLine(output);
-            string err = process.StandardError.ReadToEnd();
-            //Console.WriteLine(err);
-            ResultProcessing(output);
-            //TxtOut.AppendText(output);
-            //TxtOut.AppendText(err);
-            //showResult();
-
             process.WaitForExit();
 
         }
+        
 
         private void showResult(Data_ResultJson ResultJson)
         {
-            ResultSummary.Text = ResultJson.ReportInfo.ToSummaryString();
-
-            List<string> itemKeyList = ResultJson.ItemResults.Keys.ToList();
-            itemKeyList.Sort();
-
-            foreach(string itemName in itemKeyList)
+            Dispatcher.BeginInvoke(new Action(delegate
             {
-                ItemResultJsonData item = ResultJson.ItemResults[itemName];
-                PassStatus passStatus = PassStatus.FromString(item.PassStatus);
-                if(passStatus == PassStatus.PASS)
-                {
-                    ResultRow newRow = new ResultRow() { Item=itemName, PassStatus = passStatus.ToString(), ErrorCount="", ErrorType=""};
-                    ResultGrid.Items.Add(newRow);
-                } else if(passStatus == PassStatus.NOTPASS)
-                {
-                    foreach(TaskResultJsonData taskResult in item.CheckResults)
-                    {
-                        if (!taskResult.Pass)
-                        {
-                            ResultRow newRow = new ResultRow() { Item = itemName, PassStatus = passStatus.ToString(), ErrorCount = taskResult.errCateCount, ErrorType = taskResult.ErrorType };
-                            ResultGrid.Items.Add(newRow);
-                        }
+                ResultSummary.Text = ResultJson.ReportInfo.ToSummaryString();
+                List<string> itemKeyList = ResultJson.ItemResults.Keys.ToList();
+                itemKeyList.Sort();
 
+                foreach (string itemName in itemKeyList)
+                {
+                    ItemResultJsonData item = ResultJson.ItemResults[itemName];
+                    PassStatus passStatus = PassStatus.FromString(item.PassStatus);
+                    if (passStatus == PassStatus.PASS)
+                    {
+                        ResultRow newRow = new ResultRow() { Item = itemName, PassStatus = passStatus.ToString(), ErrorCount = "", ErrorType = "" };
+                        ResultGrid.Items.Add(newRow);
+                    }
+                    else if (passStatus == PassStatus.NOTPASS)
+                    {
+                        foreach (TaskResultJsonData taskResult in item.CheckResults)
+                        {
+                            if (!taskResult.Pass)
+                            {
+                                ResultRow newRow = new ResultRow() { Item = itemName, PassStatus = passStatus.ToString(), ErrorCount = taskResult.errCateCount, ErrorType = taskResult.ErrorType };
+                                ResultGrid.Items.Add(newRow);
+                            }
+
+                        }
                     }
                 }
-            }
+            }));
         }
 
 
